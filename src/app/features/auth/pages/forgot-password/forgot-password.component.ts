f660d2fee86a9452d3b31df608f5e209a3cfdf36
Milagros <milagrosjulisa.mm@gmail.com>
2025-06-24 18:51:31 -0500

    feat: Add user authentication pages

---
+++ src/app/features/auth/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.scss'
+})
+export class ForgotPasswordComponent {}
