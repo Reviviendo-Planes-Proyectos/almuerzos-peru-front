66258f77e2303ed85e093602a506f6c8592c0e70
Milagros <milagrosjulisa.mm@gmail.com>
2025-06-23 20:15:05 -0500

    feat: Add Angular Material configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MATERIAL_MODULES } from './shared/material.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ...MATERIAL_MODULES],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'almuerzos-peru-front';
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,4 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+export const MATERIAL_MODULES = [MatButtonModule, MatTooltipModule];
