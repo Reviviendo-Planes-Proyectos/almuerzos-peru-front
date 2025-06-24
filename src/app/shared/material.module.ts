b2bc61ef79c8baeb92a8c2ec60e7726cadd7116c
Milagros <milagrosjulisa.mm@gmail.com>
2025-06-24 04:38:03 -0500

    style: Fonts, Tailwind CSS, and Global Styles configuration

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,4 +1,5 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
 
-export const MATERIAL_MODULES = [MatButtonModule, MatTooltipModule];
+export const MATERIAL_MODULES = [MatButtonModule, MatTooltipModule, MatCardModule];
