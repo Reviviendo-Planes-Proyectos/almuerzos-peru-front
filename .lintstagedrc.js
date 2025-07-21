094a3207dcb7ad2ae02a6cd5549ff0e677eb6cac
Milagros <milagrosjulisa.mm@gmail.com>
2025-07-20 21:21:22 -0500

    refactor: Optimize lint-staged configuration and update biome settings for better file handling

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -11,7 +11,17 @@ module.exports = {
           !f.includes('jest.config')
       )
       .map((f) => `biome lint ${f}`),
-  '**/*.json': ['biome format --write'],
+  '**/*.json': (files) =>
+    files
+      .filter(
+        (f) =>
+          !f.includes('package-lock.json') &&
+          !f.includes('tsconfig') &&
+          f.includes('package.json') || 
+          f.includes('angular.json') ||
+          f.includes('biome.json')
+      )
+      .map((f) => `biome format --write ${f}`),
   '**/*.md': ['prettier --write'],
   '**/*.{html,scss}': (files) => files.map((f) => `prettier --write ${f}`),
 };
