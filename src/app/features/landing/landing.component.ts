c27a192fb8e09863a3e2a16abca84964bd9575ea
Milagros <milagrosjulisa.mm@gmail.com>
2025-07-21 14:50:24 -0500

    refactor: Remove PWA prompt component from landing page

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PwaPromptComponent } from '../../shared/components/pwa-prompt/pwa-prompt.component';
 import { MaterialModule } from '../../shared/material.module';
 import { BenefitsSectionComponent } from './components/benefits-section/benefits-section.component';
 import { FinalCtaSectionComponent } from './components/final-cta-section/final-cta-section.component';
@@ -24,7 +23,6 @@ import { TestimonialsSectionComponent } from './components/testimonials-section/
     TestimonialsSectionComponent,
     PricingSectionComponent,
     FinalCtaSectionComponent,
-    PwaPromptComponent,
     MaterialModule
   ]
 })
