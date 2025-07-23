76024f53c83110c819d98648fccde94858aa10af
Milagros <milagrosjulisa.mm@gmail.com>
2025-07-23 09:41:41 -0500

    fix: Adjust indentation and formatting in the commitlint configuration file

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-	extends: ["@commitlint/config-conventional"],
-	rules: {
-		"body-leading-blank": [2, "always"],
-		"body-max-line-length": [2, "always", 200],
-		"header-max-length": [2, "always", 200],
-		"subject-case": [2, "always", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
-		"subject-empty": [2, "never"],
-		"subject-full-stop": [2, "never", "."],
-		"type-case": [2, "always", "lower-case"],
-		"type-empty": [2, "never"],
-	},
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 200],
+    'header-max-length': [2, 'always', 200],
+    'subject-case': [
+      2,
+      'always',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never']
+  }
 };
